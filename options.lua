e5a20a31b187215dedd42cbe0e1b057e56c9888c
liam-strand <Liam.Strand@tufts.edu>
2024-02-02 20:10:38 -0500

    copy over things

--- options.lua
+++ options.lua
@@ -2,11 +2,15 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    linebreak = true, -- breaks lines at words
+    softtabstop = 4, -- because we are not animals
+    shiftwidth = 4,
+    colorcolumn = "80",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,23 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- "loctvl842/monokai-pro.nvim",
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        filter = "spectrum",
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = false }, -- any other keyword
+          type = { italic = true }, -- (preferred) int, long, char, etc
+          storageclass = { italic = true }, -- static, register, volatile, etc
+          structure = { italic = true }, -- struct, union, enum, etc
+          parameter = { italic = true }, -- parameter pass in function
+          annotation = { italic = true },
+          tag_attribute = { italic = true }, -- attribute of tag in reactjs
+        },
+      })
+    end,
+  },
 }
